3f888b63704973d87738ac32f57b5df61d67b5be
Michael Rayner <scetils_michael@hotmail.com>
2021-02-15 12:43:39 +1100

    style: :lipstick: Attach Home link to nav logo and changed global nav styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,18 +4,22 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
-        <Image src="/logo.png" alt="" width={128} height={77} />
-      </div>
       <Link href="/">
-        <a>Home</a>
+        <a>
+          <Image src="/logo.png" alt="Home" width={128} height={77} />
+        </a>
       </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/ninjas">
-        <a>Ninja Listing</a>
-      </Link>
+      <div className="link-group">
+        <Link href="/">
+          <a className="link">Home</a>
+        </Link>
+        <Link href="/about">
+          <a className="link">About</a>
+        </Link>
+        <Link href="/ninjas">
+          <a className="link">Ninja Listing</a>
+        </Link>
+      </div>
     </nav>
   );
 };
